e4cda4a783d53602f0d5b41865974511a31a621c
Niels <nielsfreelancer7750@gmail.com>
2022-09-28 15:31:30 +0200

    Add simple map

--- imap/src/App.tsx
+++ imap/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import MapChart from "./Map";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Interactive map of Swedish Real Estate
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                Interactive map of Swedish Real Estate
+            </header>
+            <body>
+            {MapChart()}
+            </body>
+        </div>
+    );
 }
 
 export default App;
